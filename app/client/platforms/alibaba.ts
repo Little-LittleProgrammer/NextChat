9053465f64308a061899c8404282ad11f4699854
EvanWu <850123119@qq.com>
2025-03-17 18:00:14 +0800

    fix: 修复缺陷

--- app/client/platforms/alibaba.ts
+++ app/client/platforms/alibaba.ts
@@ -116,21 +116,6 @@ export class QwenApi implements LLMApi {
       messages.push({ role: v.role, content });
     }
 
-    const visionModel = isVisionModel(options.config.model);
-
-    const messages: ChatOptions["messages"] = [];
-    for (const v of options.messages) {
-      const content = (
-        visionModel
-          ? await preProcessImageContentForAlibabaDashScope(v.content)
-          : v.role === "assistant"
-          ? getMessageTextContentWithoutThinking(v)
-          : getMessageTextContent(v)
-      ) as any;
-
-      messages.push({ role: v.role, content });
-    }
-
     const shouldStream = !!options.config.stream;
     const requestPayload: RequestPayload = {
       model: modelConfig.model,
